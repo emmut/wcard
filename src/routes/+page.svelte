a9807aeb669ddf12180dcf6fbdb0436b736be262
Emil Jansson <e.jansson@icloud.com>
2023-02-04 12:10:25 +0100

    💄 Adjust gap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-neutral-200 p-6 rounded-md m-2 md:m-8">
-	<div class="grid grid-cols-1 gap-3 sm:grid-cols-[30%_auto]">
+	<div class="grid grid-cols-1 gap-4 sm:grid-cols-[30%_auto]">
 		<img
 			src={config.profilePicture}
 			alt="me"

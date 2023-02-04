789367b3af4a54c5c5bb81965aebd7b59186bae3
Emil Jansson <e.jansson@icloud.com>
2023-02-04 12:14:28 +0100

    🎨 Format files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 		/>
 		<div class="prose">
 			<h1>Welcome to SvelteKit</h1>
-      <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+			<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 		</div>
 	</div>
 </div>
